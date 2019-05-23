aa7f31f0969bfe41d9223406965f98d1450b23bf
Pav0l <pavol.karas@gmail.com>
2019-05-23 21:56:18 +0200

    Update package version, update ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    rules: {
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'windows'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-        'comma-dangle': ['error', 'never'],
-    },
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'never']
+  }
 };
